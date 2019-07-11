2f88b2ea5742a6a623f48b7560beeb22aaa6b61e
= <=>
2019-07-11 00:49:58 -0300

    React concepts: state; immutability; props; defaultProps; and Proptypes

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,18 +1,52 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import TechItem from './TechItem';
 
 class TechList extends Component {
+  static defaultProps = {};
+
+  static propTypes = {};
+
   state = {
-    techs: ["Node.js", "ReactJs", "ReactNative"]
+    newTech: '',
+    newTechPlaceholder: 'Digite uma nova tech',
+    techs: ['Node.js', 'ReactJs', 'ReactNative'],
+  };
+
+  handleInputChanged = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: '',
+    });
+  };
+
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
   };
 
   render() {
-    console.log(this.state);
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>ReactNative</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <TechItem key={tech} tech={tech} onDelete={this.handleDelete} />
+          ))}
+        </ul>
+        <input
+          type="text"
+          placeholder={this.state.newTechPlaceholder}
+          onChange={this.handleInputChanged}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
